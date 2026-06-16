222c33db7aa445d371bab7d207e6a850291ed0a9
TANISHA SATI <142687396+TanishaSati@users.noreply.github.com>
2026-06-16 12:01:17 +0530

    Change model loading paths to relative
    Updated model loading paths to relative paths for portability.

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/tanisha sati/OneDrive/Desktop/Disease Detection/saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/tanisha sati/OneDrive/Desktop/Disease Detection/saved models/heart_disease_model.sav','rb'))
+diabetes_model = pickle.load(open('saved models/diabetes_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/tanisha sati/OneDrive/Desktop/Disease Detection/saved models/parkinsons_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav', 'rb'))
+
+parkinsons_model = pickle.load(open('saved models/parkinsons_model.sav', 'rb'))
+
 
 
 
